b9a72c5405ec4d552d76997d22cd845b28df0b12
sngom1995 <ngomsamba75@gmail.com>
2023-07-15 19:05:30 +0000

    rest template builder config

--- src/main/java/sn/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
+++ src/main/java/sn/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConf
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.*;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
@@ -12,6 +14,26 @@ public class RestTemplateBuilderConfig {
 
     @Value("${rest.template.rootUrl}")
     String rootUrl;
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
+    private final OAuth2AuthorizedClientService authorizedClientService;
+
+    public RestTemplateBuilderConfig(ClientRegistrationRepository clientRegistrationRepository, OAuth2AuthorizedClientService authorizedClientService) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+        this.authorizedClientService = authorizedClientService;
+    }
+
+
+    @Bean
+    OAuth2AuthorizedClientManager authorizedClientManager(){
+        var authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials()
+                .build();
+        var authorizedClientManager  = new AuthorizedClientServiceOAuth2AuthorizedClientManager
+                (clientRegistrationRepository, authorizedClientService);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+        return authorizedClientManager;
+    }
 
     @Bean
     RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer){
